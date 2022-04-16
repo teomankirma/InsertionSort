106a438ebc398f6c9f8f1d48d7418b5955f05be9
teomankirma <83593653+teomankirma@users.noreply.github.com>
2022-04-16 19:45:45 +0300

    Insertion Sort

---
+++ InsertionSort/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace InsertionSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int[] numbers = new int[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                numbers[i] = random.Next(0, 20);
+            }
+
+            InsertionSort.Sort(numbers);
+
+            Console.Write("Insertion Sort");
+            Console.WriteLine(" ");
+            Console.WriteLine(string.Join(" | ", numbers));
+        }
+    }
+}
